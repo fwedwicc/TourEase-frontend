106cbb8119cb18332f049e78692c611628dbdff7
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-11-30 22:17:15 +0800

    "Updated App.jsx, index.css, Home.jsx, and RoutePage.jsx with new styles, components, and content."

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Home, RoutePage, About, Feedback } from './pages'
 import { Sidebar, Header } from './components'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 
 const App = () => {
-
+  const { pathname } = useLocation();
   return (
     <>
       <Header />
       <Sidebar />
-      <main className='md:pl-28 pl-4 md:pt-28 pt-20 md:p-6 p-4'>
+      <main className={`md:pl-24 pl-4 md:pt-[5.3rem] pt-20 md:p-6 p-4 ${pathname === '/' ? 'h-screen' : ''}`}>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/routes' element={<RoutePage />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section>
-      <h3 className='leading-none'>Home Page</h3>
-      <p>Home page content goes here!!!!</p>
+    <section className='flex h-full'>
+      {/* Hero & Headline */}
+      <div className='flex-grow border'>
+        <h3 className='leading-none'>Home Page</h3>
+        <p></p>
+      </div>
+      {/* Suggestions */}
+      <div className='flex flex-col gap-3 border h-full overflow-y-auto p-3 w-full max-w-[21rem] custom-scrollbar'>
+        <div className='w-full h-24 border'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus et, tempora unde numquam deserunt necessitatibus ullam ea impedit quibusdam vel dolores velit pariatur minima praesentium, quod mollitia ipsam quo veritatis.
+        </div>
+        <div className='w-full h-24 border'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus et, tempora unde numquam deserunt necessitatibus ullam ea impedit quibusdam vel dolores velit pariatur minima praesentium, quod mollitia ipsam quo veritatis.
+        </div>
+        <div className='w-full h-24 border'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus et, tempora unde numquam deserunt necessitatibus ullam ea impedit quibusdam vel dolores velit pariatur minima praesentium, quod mollitia ipsam quo veritatis.
+        </div>
+        <div className='w-full h-24 border'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus et, tempora unde numquam deserunt necessitatibus ullam ea impedit quibusdam vel dolores velit pariatur minima praesentium, quod mollitia ipsam quo veritatis.
+        </div>
+        <div className='w-full h-24 border'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus et, tempora unde numquam deserunt necessitatibus ullam ea impedit quibusdam vel dolores velit pariatur minima praesentium, quod mollitia ipsam quo veritatis.
+        </div>
+      </div>
     </section>
   )
 }
